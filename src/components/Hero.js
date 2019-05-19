956bbdcb39a8d52d58377632f5ada2e5b0d5a842
mJordan <hi@mjordan.codes>
2019-05-18 19:05:00 -0700

    setting up gsap

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import HeroImg from '../assets/hero.svg';
 
+const { TimelineMax, SlowMo } = require('gsap/all');
+
 class Hero extends Component {
   componentDidMount() {
     const clouds = [
@@ -14,7 +16,16 @@ class Hero extends Component {
       document.getElementById('Cloud-5'),
       document.getElementById('Cloud-6'),
     ];
-    console.log.apply(clouds);
+
+    for (let i = 0; i < clouds.length; i++) {
+      const cloudsAnimation = new TimelineMax({ repeat: -1, delay: 3 * i });
+      cloudsAnimation.fromTo(
+        clouds[i],
+        80,
+        { x: -2100 },
+        { x: 2000, ease: SlowMo.ease.config(0.7, 0.7, false) }
+      );
+    }
   }
 
   render() {
