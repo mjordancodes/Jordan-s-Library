075aaf91977ddfeb72ba9e7a77f732de9fe310ba
mJordan Brady <hi@mjordan.codes>
2019-05-29 21:09:02 -0700

    updating book list

--- src/books-data.js
+++ src/books-data.js
@@ -203,4 +203,550 @@ export default [
       },
     ],
   },
+  {
+    title: 'Flatland: A Romance of Many Dimensions',
+    author: {
+      first: 'Edwin A.',
+      last: 'Abbott',
+    },
+    cover: '/covers/flatland.jpg',
+    genre: 'Fiction',
+    pages: 96,
+    read: true,
+  },
+  {
+    title: 'Young Zaphod Plays It Safe',
+    author: {
+      first: 'Douglas',
+      last: 'Adams',
+    },
+    series: {
+      series: "The Hitchhiker's Guide to the Galaxy",
+      order: 0.5,
+    },
+    cover: '/covers/young-zaphod-plays-it-safe.jpg',
+    genre: 'Science Fiction',
+    pages: 11,
+    read: true,
+  },
+  {
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: {
+      first: 'Douglas',
+      last: 'Adams',
+    },
+    series: {
+      series: "The Hitchhiker's Guide to the Galaxy",
+      order: 1,
+    },
+    cover: '/covers/the-hitchhikers-guide-to-the-galaxy.jpg',
+    genre: 'Science Fiction',
+    pages: 193,
+    read: true,
+  },
+  {
+    title: 'The Restaurant at the End of the Universe',
+    author: {
+      first: 'Douglas',
+      last: 'Adams',
+    },
+    series: {
+      series: "The Hitchhiker's Guide to the Galaxy",
+      order: 2,
+    },
+    cover: '/covers/the-restaurant-at-the-end-of-the-universe.jpg',
+    genre: 'Science Fiction',
+    pages: 250,
+    read: true,
+  },
+  {
+    title: 'Life, the Universe and Everything',
+    author: {
+      first: 'Douglas',
+      last: 'Adams',
+    },
+    series: {
+      series: "The Hitchhiker's Guide to the Galaxy",
+      order: 3,
+    },
+    cover: '/covers/life-the-universe-and-everything.jpg',
+    genre: 'Science Fiction',
+    pages: 224,
+    read: true,
+  },
+  {
+    title: 'So Long, and Thanks for All the Fish',
+    author: {
+      first: 'Douglas',
+      last: 'Adams',
+    },
+    series: {
+      series: "The Hitchhiker's Guide to the Galaxy",
+      order: 4,
+    },
+    cover: '/covers/so-long-and-thanks-for-all-the-fish.jpg',
+    genre: 'Science Fiction',
+    pages: 225,
+    read: true,
+  },
+  {
+    title: 'Mostly Harmless',
+    author: {
+      first: 'Douglas',
+      last: 'Adams',
+    },
+    series: {
+      series: "The Hitchhiker's Guide to the Galaxy",
+      order: 5,
+    },
+    cover: '/covers/mostly-harmless.jpg',
+    genre: 'Science Fiction',
+    pages: 288,
+    read: true,
+  },
+  {
+    title: 'Getting Things Done: The Art of Stress-Free Productivity',
+    author: {
+      first: 'David',
+      last: 'Allen',
+    },
+    cover: '/covers/getting-things-done.jpg',
+    genre: 'Non-Fiction',
+    pages: 352,
+    read: true,
+  },
+  {
+    title: 'Flowers in the Attic',
+    author: {
+      first: 'V. C.',
+      last: 'Andrews',
+    },
+    series: {
+      series: 'Dollanganger',
+      order: 1,
+    },
+    cover: '/covers/flowers-in-the-attic.jpg',
+    genre: 'Fiction',
+    pages: 389,
+    read: true,
+  },
+  {
+    title: 'Petals on the Wind',
+    author: {
+      first: 'V. C.',
+      last: 'Andrews',
+    },
+    series: {
+      series: 'Dollanganger',
+      order: 2,
+    },
+    cover: '/covers/petals-on-the-wind.jpg',
+    genre: 'Fiction',
+    pages: 439,
+    read: true,
+  },
+  {
+    title: 'A Spell for Chameleon',
+    author: {
+      first: 'Pierz',
+      last: 'Anthony',
+    },
+    series: {
+      series: 'Xanth',
+      order: 1,
+    },
+    cover: '/covers/a-spell-for-chameleon.jpg',
+    genre: 'Fantasy',
+    pages: 344,
+    read: true,
+  },
+  {
+    title: 'The Source of Magic',
+    author: {
+      first: 'Pierz',
+      last: 'Anthony',
+    },
+    series: {
+      series: 'Xanth',
+      order: 2,
+    },
+    cover: '/covers/the-source-of-magic.jpg',
+    genre: 'Fantasy',
+    pages: 326,
+    read: true,
+  },
+  {
+    title: "Moon Shot: The Inside Story of America's Race to the Moon",
+    author: {
+      first: 'Alan',
+      last: 'Shepard, Deke Slayton',
+    },
+    cover: '/covers/moon-shot.jpg',
+    genre: 'Space History',
+    pages: 383,
+    read: false,
+  },
+  {
+    title: 'Peter Pan',
+    author: {
+      first: 'J. M.',
+      last: 'Barrie',
+    },
+    cover: '/covers/peter-pan.jpg',
+    genre: 'Fantasy',
+    pages: 256,
+    read: true,
+  },
+  {
+    title: 'The Looking Glass Wars',
+    author: {
+      first: 'Frank',
+      last: 'Beddor',
+    },
+    series: {
+      series: 'The Looking Glass Wars',
+      order: 1,
+    },
+    cover: '/covers/the-looking-glass-wars.jpg',
+    genre: 'Fantasy',
+    pages: 364,
+    read: true,
+  },
+  {
+    title: 'Seeing Redd',
+    author: {
+      first: 'Frank',
+      last: 'Beddor',
+    },
+    series: {
+      series: 'The Looking Glass Wars',
+      order: 2,
+    },
+    cover: '/covers/seeing-redd.jpg',
+    genre: 'Fantasy',
+    pages: 371,
+    read: true,
+  },
+  {
+    title: 'ArchEnemy',
+    author: {
+      first: 'Frank',
+      last: 'Beddor',
+    },
+    series: {
+      series: 'The Looking Glass Wars',
+      order: 3,
+    },
+    cover: '/covers/archenemy.jpg',
+    genre: 'Fantasy',
+    pages: 370,
+    read: true,
+  },
+  {
+    title: 'Alice I Have Been',
+    author: {
+      first: 'Melanie',
+      last: 'Benjamin',
+    },
+    cover: '/covers/alice-i-have-been.jpg',
+    genre: 'Historical Fiction',
+    pages: 345,
+    read: false,
+  },
+  {
+    title: 'Fahrenheit 451',
+    author: {
+      first: 'Ray',
+      last: 'Bradbury',
+    },
+    cover: '/covers/fahrenheit-451.jpg',
+    genre: 'Science Fiction',
+    pages: 194,
+    read: true,
+  },
+  {
+    title: 'The Martian Chronicles',
+    author: {
+      first: 'Ray',
+      last: 'Bradbury',
+    },
+    cover: '/covers/the-martian-chronicles.jpg',
+    genre: 'Science Fiction',
+    pages: 182,
+    read: true,
+  },
+  {
+    title: 'Pride and Prejudice and Zombies',
+    author: {
+      first: 'Seth',
+      last: 'Grahame-Smith, Jane Austen',
+    },
+    series: {
+      series: 'Pride and Prejudice and Zombies',
+      order: 1,
+    },
+    cover: '/covers/pride-prejudice-zombies.jpg',
+    genre: 'Fiction',
+    pages: 320,
+    read: true,
+  },
+  {
+    title: 'CSS Animations and Transitions for the Modern Web ',
+    author: {
+      first: 'Steven',
+      last: 'Bradley',
+    },
+    cover: '/covers/css-animations-and-transitions.jpg',
+    genre: 'Technical',
+    pages: 320,
+    read: true,
+  },
+  {
+    title:
+      'Welcome to the Goddamn Ice Cube: Chasing Fear and Finding Home in the Great White North ',
+    author: {
+      first: 'Blair',
+      last: 'Braverman',
+    },
+    cover: '/covers/welcome-to-the-goddamn-ice-cube.jpg',
+    genre: 'Non-Fiction',
+    pages: 274,
+    read: false,
+  },
+  {
+    title: 'The Lost Symbol',
+    author: {
+      first: 'Dan',
+      last: 'Brown',
+    },
+    series: {
+      series: 'Robert Langdon',
+      order: 3,
+    },
+    cover: '/covers/the-lost-symbol.jpg',
+    genre: 'Fiction',
+    pages: 509,
+    read: false,
+  },
+  {
+    title: 'The Miniaturist',
+    author: {
+      first: 'Jessie',
+      last: 'Burton',
+    },
+    cover: '/covers/the-miniaturist.jpg',
+    genre: 'Historical Fiction',
+    pages: 400,
+    read: false,
+  },
+  {
+    title: 'Storm Front',
+    author: {
+      first: 'Jim',
+      last: 'Butcher',
+    },
+    series: {
+      series: 'The Dresden Files',
+      order: 1,
+    },
+    cover: '/covers/storm-front.jpg',
+    genre: 'Fantasy',
+    pages: 322,
+    read: true,
+  },
+  {
+    title: 'Shadowland',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: 'Mediator',
+      order: 1,
+    },
+    cover: '/covers/shadowland.jpg',
+    genre: 'Young Adult',
+    pages: 287,
+    read: true,
+  },
+  {
+    title: 'Ninth Key',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: 'Mediator',
+      order: 2,
+    },
+    cover: '/covers/ninth-key.jpg',
+    genre: 'Young Adult',
+    pages: 287,
+    read: true,
+  },
+  {
+    title: 'Reunion',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: 'Mediator',
+      order: 3,
+    },
+    cover: '/covers/reunion.jpg',
+    genre: 'Young Adult',
+    pages: 289,
+    read: true,
+  },
+  {
+    title: 'Darkest Hour',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: 'Mediator',
+      order: 4,
+    },
+    cover: '/covers/darkest-hour.jpg',
+    genre: 'Young Adult',
+    pages: 316,
+    read: true,
+  },
+  {
+    title: 'Haunted',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: 'Mediator',
+      order: 5,
+    },
+    cover: '/covers/haunted.jpg',
+    genre: 'Young Adult',
+    pages: 263,
+    read: true,
+  },
+  {
+    title: 'Twilight',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: 'Mediator',
+      order: 6,
+    },
+    cover: '/covers/twilight.jpg',
+    genre: 'Young Adult',
+    pages: 312,
+    read: true,
+  },
+  {
+    title: 'Proposal',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: 'Mediator',
+      order: 6.5,
+    },
+    cover: '/covers/proposal.jpg',
+    genre: 'Young Adult',
+    pages: 144,
+    read: true,
+  },
+  {
+    title: 'Remembrance',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: 'Mediator',
+      order: 7,
+    },
+    cover: '/covers/remembrance.jpg',
+    genre: 'Young Adult',
+    pages: 388,
+    read: true,
+  },
+  {
+    title: 'When Lightning Strikes',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: '1-800-Where-R-You',
+      order: 1,
+    },
+    cover: '/covers/when-lightning-strikes.jpg',
+    genre: 'Young Adult',
+    pages: 266,
+    read: true,
+  },
+  {
+    title: 'Code Name Cassandra',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: '1-800-Where-R-You',
+      order: 2,
+    },
+    cover: '/covers/code-name-cassandra.jpg',
+    genre: 'Young Adult',
+    pages: 264,
+    read: true,
+  },
+  {
+    title: 'Safe House',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: '1-800-Where-R-You',
+      order: 3,
+    },
+    cover: '/covers/safe-house.jpg',
+    genre: 'Young Adult',
+    pages: 262,
+    read: true,
+  },
+  {
+    title: 'Sanctuary',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: '1-800-Where-R-You',
+      order: 4,
+    },
+    cover: '/covers/sanctuary.jpg',
+    genre: 'Young Adult',
+    pages: 231,
+    read: true,
+  },
+  {
+    title: 'Missing You',
+    author: {
+      first: 'Meg',
+      last: 'Cabot',
+    },
+    series: {
+      series: '1-800-Where-R-You',
+      order: 5,
+    },
+    cover: '/covers/missing-you.jpg',
+    genre: 'Young Adult',
+    pages: 268,
+    read: true,
+  },
 ];
